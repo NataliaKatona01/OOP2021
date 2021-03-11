0d67b447b81ed887ccac9a4d72db55cc47e10ef7
Natalia Katona <katonanatalia6@gmail.com>
2021-03-11 12:06:05 +0200

    numere rationale

---
+++ numere_rationale/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numere_rationale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational r1 = new Rational(12, 5);
+            Console.WriteLine(r1);
+            Rational r2 = new Rational(-20, -3);
+            Console.WriteLine(r2);
+            Rational r3 = new Rational(4, 7);
+
+            Rational sum = r1.Add(r2);
+            Console.WriteLine($"{r1} + {r2} = {sum}");
+
+            Rational multiply = r1.Multiply(r2);
+            Console.WriteLine($"{r1} * {r2} = {multiply}");
+
+            Rational devide = r1.Devide(r2);
+            Console.WriteLine($"{r1} / {r2} = {devide}");
+        }
+       
+    }
+}
---
+++ numere_rationale/Rational.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace numere_rationale
+{
+    internal class Rational
+    {
+        private int a;
+        private int b;
+
+        public Rational(int a, int b)
+        {
+         
+            if (b < 0)
+            {
+                a *= -1;
+                b *= -1;
+            }
+            this.a = a;
+            this.b = b;
+            Simplify();
+        }
+
+        public override string ToString()
+        {
+            if (a == 0)
+                return "0";
+            if (b == 0)
+                return "Nu exista";
+            if (b == 1)
+                return a.ToString();
+            return a.ToString() + "/" + b.ToString();
+        }
+
+        public Rational Add(Rational r2)
+        {
+            if (r2.b != b)
+                return new Rational(r2.a * b + a * r2.b, r2.b * b);
+            else
+                return new Rational(r2.a + a, b);
+        }
+        private void Simplify()
+        {
+            if (a != 0)
+            {
+                int cmmdc = Cmmdc(Math.Abs(a), Math.Abs(b));
+
+                a /= cmmdc;
+                b /= cmmdc;
+            }
+        }
+        public Rational Subtract(Rational r2)
+        {
+            if (r2.b != b)
+                return new Rational(r2.a * b - a * r2.b, r2.b * b);
+            else
+                return new Rational(r2.a - a, b);
+        }
+
+        public Rational Multiply(Rational r2)
+        {
+            return new Rational(r2.a * a, r2.b * b);
+        }
+
+        public Rational Devide(Rational r2)
+        {
+            return new Rational(a * r2.b, b * r2.a);
+        }
+        public int Cmmdc(int x, int y)
+        {
+            while (x != y)
+                if (x > y)
+                    x -= y;
+                else
+                    y -= x;
+            return x;
+        }
+    }
+}
